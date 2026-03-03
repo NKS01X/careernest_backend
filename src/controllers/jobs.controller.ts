a7d9ccfa90df8ba52548392b756aeab7e225a05d
NKS01X <nikhilayush2020@gmail.com>
2026-03-04 01:53:44 +0530

    added job routes and rendering of jobs according to role

---
+++ src/controllers/jobs.controller.ts
@@ -0,0 +1,73 @@
+import type { Request, Response } from "express";
+import prisma from "../lib/db.js";
+
+export const getAllJobs = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const jobs = await prisma.job.findMany({
+      include: { createdBy: { select: { name: true } } },
+      orderBy: { createdAt: 'desc' }
+    });
+    return res.status(200).json({ success: true, jobs });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Error fetching jobs" });
+  }
+};
+
+export const getJobById = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const id = req.params.id as string;
+    
+    const job = await prisma.job.findUnique({
+      where: { id },
+      include: { createdBy: { select: { name: true } } }
+    });
+    if (!job) return res.status(404).json({ message: "Job not found" });
+    return res.status(200).json({ success: true, job });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Error fetching job" });
+  }
+};
+
+export const getMyJobs = async (req: any, res: Response): Promise<any> => {
+  try {
+    const userId = req.user.id;
+    const role = req.user.role;
+
+    let jobs;
+    if (role === "Recruiter") {
+      jobs = await prisma.job.findMany({ 
+        where: { createdById: userId } 
+      });
+    } else {
+      jobs = await prisma.application.findMany({
+        where: { userId },
+        include: { job: true }
+      });
+    }
+    return res.status(200).json({ success: true, jobs });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Error fetching your jobs" });
+  }
+};
+
+export const createJob = async (req: any, res: Response): Promise<any> => {
+  try {
+    if (req.user.role !== "Recruiter") {
+      return res.status(403).json({ message: "Only recruiters can post jobs" });
+    }
+    const { title, company, description, location, salary } = req.body;
+    const newJob = await prisma.job.create({
+      data: { 
+        title, 
+        company, 
+        description, 
+        location, 
+        salary, 
+        createdById: req.user.id 
+      }
+    });
+    return res.status(201).json({ success: true, job: newJob });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Error creating job" });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { register } from "./controllers/register.controller.js";
 import { login } from "./controllers/login.controller.js";
 import { logout } from "./controllers/logout.controller.js";
 import { isLoggedIn } from "./middleware/auth.js";
+import { getAllJobs, getJobById, getMyJobs, createJob } from "./controllers/jobs.controller.js"
 
 import type { Application, Request, Response } from 'express';
 
@@ -12,6 +13,16 @@ dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT||3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('WELCOME TO Careernest!');
+});
+// Auth Routes
+app.post("/login", login);
+app.post("/register", register);
+app.post("/logout", isLoggedIn, logout);
+
+// Job Routes
 /*
 
 GET /jobs        → all jobs (for students)
@@ -19,12 +30,11 @@ GET /jobs/:id    → specific job
 
 GET /jobs/my     → jobs creates by the current user or maybe ?createdBy=me
 */
-app.get('/', (req: Request, res: Response) => {
-  res.send('WELCOME TO INTERNSHILA!');
-});
-app.post("/login", login);
-app.post("/register", register);
-app.post("/logout", isLoggedIn, logout);
+app.get("/jobs", getAllJobs);
+app.get("/jobs/my", isLoggedIn, getMyJobs); // Protected: needs user ID from token
+app.get("/jobs/:id", getJobById);
+app.post("/jobs", isLoggedIn, createJob); // Protected: only recruiters
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
